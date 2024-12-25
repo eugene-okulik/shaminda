aa65b8a91bbc8d0d43f94ea749863660861b28ca
Daniil Shamin <a123@MacBook-Air-123.local>
2024-12-26 01:06:27 +0300

    home_work_lesson_5

--- homework/eugene_okulik/home_work_lesson_5/home_work.py
+++ homework/eugene_okulik/home_work_lesson_5/home_work.py
@@ -1,7 +1,6 @@
 person = ['John', 'Doe', 'New York', '+1372829383739', 'US']
 name, last_name, city, phone, country = person
-print(name,last_name,city,phone,country)
-
+print(name, last_name, city, phone, country)
 
 one_value = 'результат операции: 42'
 new_one_value = one_value.index(':') + 2
@@ -11,7 +10,6 @@ if number_str.isdigit():
     result = number + 10
 print(result)
 
-
 two_value = 'результат операции: 514'
 new_two_value = two_value.index(':') + 2
 number_str_two = two_value[new_two_value:]
@@ -19,7 +17,6 @@ if number_str_two.isdigit():
     number_two = int(number_str_two)
     uspex = number_two + 10
 print(uspex)
-
 
 three_value = 'результат работы программы: 9'
 new_three_value = three_value.index(':') + 2
@@ -29,6 +26,13 @@ if number_str_three.isdigit():
     result_two = number_three + 10
 print(result_two)
 
+students = ['Ivanov', 'Petrov', 'Sidorov']
+subjects = ['math', 'biology', 'geography']
+
+students_str = ', '.join(students)
+subjects_str = ', '.join(subjects)
+
+print(f'Students {students_str} study these subjects: {subjects_str}')
 
 students = ['Ivanov', 'Petrov', 'Sidorov']
 subjects = ['math', 'biology', 'geography']
@@ -38,17 +42,9 @@ new_str_three = str(students[2])
 subjects_one = str(subjects[0])
 subjects_two = str(subjects[1])
 subjects_three = str(subjects[2])
+
 print(f'Students {new_str}, {new_str_two}, {new_str_three} study these subjects: {subjects_one}, {subjects_two}, {subjects_three} ')
-## сначало сделал без джойнов , попробовал получилось, но запись длинная и не особо читаема
-
-
-students = ['Ivanov', 'Petrov', 'Sidorov']
-subjects = ['math', 'biology', 'geography']
-
-students_str = ', '.join(students)
-subjects_str = ', '.join(subjects)
-
-print(f'Students {students_str} study these subjects: {subjects_str}')
+#сначало сделал без джойнов , попробовал получилось, но запись длинная и не особо читаема
 
 
 
