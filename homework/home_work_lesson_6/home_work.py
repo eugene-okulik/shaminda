c19e029b5d346a6da856c784d2a5cba25c5ca35e
Daniil Shamin <a123@MacBook-Air-123.local>
2024-12-27 01:05:49 +0300

    Home_work_6

--- homework/home_work_lesson_6/home_work.py
+++ homework/home_work_lesson_6/home_work.py
@@ -1,14 +1,17 @@
 
-random_text = 'Etiam tincidunt neque erat, quis molestie enim imperdiet vel. Integer urna nisl, facilisis vitae semper at, dignissim vitae libero'
+random_text = (
+    'Etiam tincidunt neque erat, quis molestie enim imperdiet vel. '
+    'Integer urna nisl, facilisis vitae semper at, dignissim vitae libero'
+)
 new_text = []
 
 for text in random_text.split():
     if text.endswith(','):
-       new_text.append(text[:-1] + 'ing')
+        new_text.append(text[:-1] + 'ing')
     elif text.endswith('.'):
         new_text.append(text[:-1] + 'ing')
     else:
         new_text.append(text + 'ing')
-new_tex_string = ' '.join(new_text)
 
-print(new_tex_string)
+new_text_string = ' '.join(new_text)
+print(new_text_string)
