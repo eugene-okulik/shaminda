f3fa51175d8de69656c54c07d7c25459c02c9f1d
Daniil Shamin <a123@MacBook-Air-123.local>
2025-01-11 18:32:42 +0300

    Home_work_13

---
+++ homework/eugene_okulik/дз lesson3/homework13.py
@@ -0,0 +1,44 @@
+import datetime
+import re
+
+file_path = '../hw_13/data.txt'
+
+def read_file():
+    with open(file_path, 'r') as file:
+        for line in file.readlines():
+            process_line(line.strip())
+
+def process_line(line):
+    parts = line.split(' - ')
+    if len(parts) < 2:
+        return
+
+    number = parts[0].strip()
+    text = parts[1].strip()
+
+
+    date_match = re.search(r'(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d+)', line)
+
+    if not date_match:
+        print(f"Ошибка: дата не найдена в строке '{line}'")
+        return
+
+    date_str = date_match.group(1)
+
+
+    date_obj = datetime.datetime.strptime(date_str, '%Y-%m-%d %H:%M:%S.%f')
+
+    if number.startswith('1'):
+        new_date = date_obj + datetime.timedelta(weeks=1)
+        print(f"Номер {number}: {new_date}")
+
+    elif number.startswith('2'):
+        day_of_week = date_obj.strftime('%A')
+        print(f"Номер {number}: {day_of_week}")
+
+    elif number.startswith('3'):
+        days_ago = (datetime.datetime.now() - date_obj).days
+        print(f"Номер {number}: {days_ago} дней назад")
+
+# Читаем файл и обрабатываем его содержимое
+read_file()
