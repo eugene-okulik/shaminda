124f3b9485b9d605cd248482e0c268c4a2c5f451
daniil_shamin <shamindaniil1129@icloud.com>
2025-04-08 01:49:05 +0300

    Home_work_21.5

--- homework/eugene_okulik/test_api_uniquename/tests/test_api.py
+++ homework/eugene_okulik/test_api_uniquename/tests/test_api.py
@@ -1,31 +1,6 @@
 
-import pytest
-from test_api_uniquename.endpoints.create_object import CreateObject
 from test_api_uniquename.endpoints.get_object import GetObject
 from test_api_uniquename.endpoints.delete_object import DeleteObject
-
-
-@pytest.fixture()
-def create_and_delete_object():
-    obj = CreateObject()
-    body = {
-        "name": "test_objected",
-        "data": {"color": "black", "size": "big"}
-    }
-    obj.create_object(body)
-    obj.assert_response()
-    object_id = obj.get_object_id()
-
-    yield obj, object_id
-
-    get_obj = GetObject()
-    try:
-        get_obj.get_object(object_id)
-        delete_obj = DeleteObject()
-        delete_obj.delete_object(object_id)
-        delete_obj.assert_response()
-    except Exception as e:
-        print(f"Ошибка при удалении объекта: {e}")
 
 
 def test_create_object(create_and_delete_object):
