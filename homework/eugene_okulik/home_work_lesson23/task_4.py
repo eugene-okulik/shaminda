c4e0a24c3056f1b1554423e870952a7ffa782eb0
a123 <samindaniil97@gmail.com>
2025-06-12 01:38:31 +0300

    Home_work_23 - ui

---
+++ homework/eugene_okulik/home_work_lesson23/task_4.py
@@ -0,0 +1,27 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from time import sleep
+import pytest
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+@pytest.fixture
+def driver_with_browser_2():
+    chrome_driver = webdriver.Chrome()
+    chrome_driver.maximize_window()
+    yield chrome_driver
+    sleep(5)
+
+
+def test_start(driver_with_browser_2):
+    driver_with_browser_2.get("https://the-internet.herokuapp.com/dynamic_loading/2")
+    booton = driver_with_browser_2.find_element(By.XPATH,"//button[text()='Start']")
+    booton.click()
+    WebDriverWait(driver_with_browser_2, 15).until(
+        EC.visibility_of_element_located((By.ID, "finish"))
+    )
+    finish_text = driver_with_browser_2.find_element(By.ID, "finish").text
+    assert finish_text == "Hello World!", (
+        f"Test failed: expected 'Hello World!', got '{finish_text}'"
+    )
