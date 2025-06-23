bb23656e35a5dace641a65adeac6d948b66abff5
a123 <samindaniil97@gmail.com>
2025-06-23 22:47:30 +0300

    Home_work_23 - ui

--- homework/eugene_okulik/home_work_lesson23/task_3.py
+++ homework/eugene_okulik/home_work_lesson23/task_3.py
@@ -15,9 +15,12 @@ def driver_with_browser_2():
 
 def test_first(driver_with_browser_2):
     driver_with_browser_2.get("https://www.qa-practice.com/elements/select/single_select")
+
     select = Select(driver_with_browser_2.find_element(By.ID, "id_choose_language"))
     select.select_by_value("1")
+
     submit = driver_with_browser_2.find_element(By.ID, "submit-id-submit")
     submit.click()
-    result = driver_with_browser_2.find_element(By.XPATH,"//p[@id='result-text']")
+
+    result = driver_with_browser_2.find_element(By.XPATH, "//p[@id='result-text']")
     assert result.text == "Python"
--- homework/eugene_okulik/home_work_lesson23/task_4.py
+++ homework/eugene_okulik/home_work_lesson23/task_4.py
@@ -16,11 +16,14 @@ def driver_with_browser_2():
 
 def test_start(driver_with_browser_2):
     driver_with_browser_2.get("https://the-internet.herokuapp.com/dynamic_loading/2")
-    booton = driver_with_browser_2.find_element(By.XPATH,"//button[text()='Start']")
-    booton.click()
+
+    button = driver_with_browser_2.find_element(By.XPATH, "//button[text()='Start']")
+    button.click()
+
     WebDriverWait(driver_with_browser_2, 15).until(
         EC.visibility_of_element_located((By.ID, "finish"))
     )
+
     finish_text = driver_with_browser_2.find_element(By.ID, "finish").text
     assert finish_text == "Hello World!", (
         f"Test failed: expected 'Hello World!', got '{finish_text}'"
