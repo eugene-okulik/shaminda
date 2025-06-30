8504d584b3ec811e4af4f32a57b8c0270ca238a8
a123 <samindaniil97@gmail.com>
2025-07-01 02:05:28 +0300

    Home_work_24 - ui

---
+++ homework/eugene_okulik/Home_Work_24/task_1.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import pytest
+
+
+@pytest.fixture
+def driver():
+    chrome_driver = webdriver.Chrome()
+    chrome_driver.maximize_window()
+    yield chrome_driver
+
+
+def test_one(driver):
+    driver.get("https://www.demoblaze.com/index.html")
+    driver.execute_script("window.open('https://www.demoblaze.com/prod.html?idp_=1', '_blank');")
+    driver.switch_to.window(driver.window_handles[1])
+    driver.find_element(By.XPATH,"//div[@class='col-sm-12 col-md-6 col-lg-6']"
+                                 "/a[@onclick='addToCart(1)']").click()
+    allert = driver.switch_to.alert
+    allert.accept()
+    driver.close()
+    driver.switch_to.window(driver.window_handles[0])
+    driver.find_element(By.XPATH,"//a[@id='cartur' and @class='nav-link']").click()
+    text_add = driver.find_element(By.XPATH,"//td[text()='Samsung galaxy s6']")
+    assert text_add.text == "Samsung galaxy s6"
---
+++ homework/eugene_okulik/Home_Work_24/task_2.py
@@ -0,0 +1,40 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+@pytest.fixture
+def driver():
+    chrome_driver = webdriver.Chrome()
+    chrome_driver.maximize_window()
+    yield chrome_driver
+
+
+def test_compare_product(driver):
+    driver.get("https://magento.softwaretestingboard.com/gear/bags.html")
+    first_product = WebDriverWait(driver, 10).until(
+        EC.visibility_of_element_located((By.XPATH, "//img[@class='product-image-photo' and @src='https://magento.softwaretestingboard.com/pub/media/catalog/product/cache/7c4c1ed835fbbf2269f24539582c6d44/w/b/wb04-blue-0.jpg']"))
+    )
+    ActionChains(driver).move_to_element(first_product).perform()
+    add_to_compare_button = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, "//button[@class='action tocart primary']"))
+    )
+    add_to_compare_button.click()
+    compare_section = WebDriverWait(driver, 10).until(
+        EC.visibility_of_element_located((By.XPATH,
+                                          "//strong[@id='block-compare-heading' and text()='Compare Products']"))
+    )
+    # По нажатии add_to_compare_button.click() появляется ошибка The requested qty is not available и переход
+    # на страницу с ошибкой , с остальными товарами такого нет, но поле  compare_products не меняется при нажатии
+    # на них, обьясните может я не так понял задачу, но после нажатия кнопки должно изменится  compare_products
+    # а не появится ошибка не дающая сделать что либо
+    # по этому дальше тест дописывал абстракто что все работает
+    compare_products = compare_section.find_elements(By.XPATH,
+                                                     "//div[@class='empty' and text()='You have no items to compare.']")
+    assert len(compare_products) > 0, "Товар не добавлен в секцию сравнения."
+    # Выводим название первого товара в секции сравнения
+    product_name = compare_products[0].find_element(By.XPATH, ".product-item-name").text
+    print(f"Товар в секции сравнения: {product_name}")
