8929303ac74529dcef9655fd011adbfb0e7bd01a
a123 <samindaniil97@gmail.com>
2025-07-03 00:46:38 +0300

    Home_work_24 - ui

--- homework/eugene_okulik/Home_Work_24/task_2.py
+++ homework/eugene_okulik/Home_Work_24/task_2.py
@@ -16,27 +16,25 @@ def driver():
 def test_compare_product(driver):
     driver.get("https://magento.softwaretestingboard.com/gear/bags.html")
     first_product = WebDriverWait(driver, 10).until(
-        EC.visibility_of_element_located((By.XPATH, "//li[@class='item product product-item']"))
+        EC.visibility_of_element_located((By.XPATH, "//img[@class='product-image-photo'"
+                                                    " and @alt='Push It Messenger Bag']"))
     )
 
     ActionChains(driver).move_to_element(first_product).perform()
     add_to_compare_button = WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.XPATH, "//button[@class='action tocart primary']"))
+        EC.element_to_be_clickable((By.XPATH, "//a[@class='action tocompare' and @title='Add to Compare']"))
     )
     add_to_compare_button.click()
     compare_section = WebDriverWait(driver, 10).until(
         EC.visibility_of_element_located((By.XPATH,
                                           "//strong[@id='block-compare-heading' and text()='Compare Products']"))
     )
-    # По нажатии add_to_compare_button.click() появляется ошибка The requested qty is not available и переход
-    # на страницу с ошибкой , с остальными товарами такого нет, но поле  compare_products не меняется при нажатии
-    # на них, обьясните может я не так понял задачу, но после нажатия кнопки должно изменится  compare_products
-    # а не появится ошибка не дающая сделать что либо
-    # по этому дальше тест дописывал абстракто что все работает
+
     compare_products = compare_section.find_elements(By.XPATH,
-                                                     "//div[@class='empty'"
-                                                     " and text()='You have no items to compare.']")
-    assert len(compare_products) > 0, "Товар не добавлен в секцию сравнения."
-    # Выводим название первого товара в секции сравнения
-    product_name = compare_products[0].find_element(By.XPATH, ".product-item-name").text
-    print(f"Товар в секции сравнения: {product_name}")
+                                                     "//li[contains(@class, 'product-item')]//a[text()='Push It Messenger Bag']")
+
+    assert len(compare_products) > 0, "Product 'Push It Messenger Bag' not found in compare list."
+
+    assert compare_products[0].text == "Push It Messenger Bag", "Product name does not match."
+    #без впн не открывает сайт, под впн выпадает какая то реклама которой нет при запуске просто в браузере
+    #пытался как то воспроизвести рекламу в браузере не вышло,тест как пройти понимаю но реклама не дает
